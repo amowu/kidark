d2f525bade819704dd789e787c3e2afa56a2e3f0
Amo Wu <amo260@gmail.com>
2015-11-03 01:53:36 +0800

    Refactor App component

--- src/client/app/App.react.js
+++ src/client/app/App.react.js
@@ -1,18 +1,15 @@
-import './App.styl'
+import React, { PropTypes } from 'react'
 import Component from 'react-pure-render/component'
-import Footer from './Footer.react'
-import Header from './Header.react'
-import React, {PropTypes} from 'react'
-import RouterHandler from '../../common/components/RouterHandler.react'
-import mapDispatchToProps from '../../common/app/mapDispatchToProps'
-import mapStateToProps from '../../common/app/mapStateToProps'
-import {connect} from 'react-redux'
+import { connect } from 'react-redux'
 
+import mapStateToProps from '../../common/app/mapStateToProps'
+import mapDispatchToProps from '../../common/app/mapDispatchToProps'
+import RouterHandler from '../../common/components/RouterHandler.react'
+import Header from './Header.react'
 import Game from './Game.react'
 
 @connect(mapStateToProps, mapDispatchToProps)
 export default class App extends Component {
-
   static propTypes = {
     children: PropTypes.object.isRequired,
     location: PropTypes.object.isRequired,
@@ -21,18 +18,18 @@ export default class App extends Component {
   }
 
   render () {
-    const {location: {pathname}, msg, users: {viewer}} = this.props
+    const {
+      location: { pathname },
+      msg,
+      users: { viewer }
+    } = this.props
 
     return (
-      // Pass data-pathname to allow route specific styling.
       <div className='page' data-pathname={pathname}>
-        {/* Pathname enforces rerender so activeClassName is updated. */}
         <Header msg={msg} pathname={pathname} viewer={viewer} />
         <Game />
         <RouterHandler {...this.props} />
-        <Footer msg={msg.app.footer} />
       </div>
     )
   }
-
 }
