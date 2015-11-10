248bf08ed498a59651d9535345fed8330340fede
Amo Wu <amo260@gmail.com>
2015-11-10 18:41:52 +0800

    Remove serve-favicon

--- src/server/frontend/index.js
+++ src/server/frontend/index.js
@@ -2,15 +2,12 @@ import compression from 'compression'
 import device from 'express-device'
 import esteMiddleware from '../lib/esteMiddleware'
 import express from 'express'
-// import favicon from 'serve-favicon'
 import render from './render'
 
 const app = express()
 
 app.use(esteMiddleware())
 app.use(compression())
-
-// app.use(favicon('assets/img/favicon.ico'))
 
 // Serve the static assets. We can cache them as they include hashes.
 app.use('/assets/img', express.static('assets/img', {maxAge: '200d'}))
