e9c4c8686df476d93d2a8f6a0b07bdf3297a86e8
Amo Wu <amo260@gmail.com>
2015-12-09 02:24:57 +0800

    Add new Resume component to test react-toolbox

---
+++ src/browser/Resume/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Component from 'react-pure-render/component'
+import { Card, CardTitle } from 'react-toolbox/lib/card'
+
+export default class Resume extends Component {
+  render () {
+    return (
+      <Card>
+        <CardTitle title='Resume' />
+      </Card>
+    )
+  }
+}
