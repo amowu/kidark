e7c16456fd1ae949d054baa365ba141ef2f56492
Amo Wu <amo260@gmail.com>
2015-12-11 17:19:31 +0800

    Layout Resume

--- src/browser/Resume/Resume.jsx
+++ src/browser/Resume/Resume.jsx
@@ -1,5 +1,5 @@
 import React, { PropTypes } from 'react'
-import { Button } from 'react-bootstrap'
+import { Image, Panel, Tab, Tabs } from 'react-bootstrap'
 import Component from 'react-pure-render/component'
 
 import { fetchUser } from '../../common/users/users.actions.js'
@@ -21,11 +21,63 @@ export default class Resume extends Component {
     // TODO: Use :username or users.current
     if (!entities.has('amowu')) return <p>fetching user...</p>
 
+    const firstName = entities.getIn(['amowu', 'firstName'])
+    const lastName = entities.getIn(['amowu', 'lastName'])
+
     return (
-      <div>
-        <div>{entities.getIn(['amowu', 'firstName'])}</div>
-        <Button bsStyle='primary'>Primary</Button>
-      </div>
+      <Panel>
+        <div className='resume-profile'>
+          <div>
+            <Image src='http://www.gravatar.com/avatar/778858b33950746e7049bfc33e94ac50?s=128' />
+            <h1>{firstName} {lastName}</h1>
+          </div>
+          <div>
+            <p>Front-end Engineer</p>
+          </div>
+          <div>
+            <dl>
+              <dt>Location</dt>
+              <dd>New Taipei City, Taiwan</dd>
+              <dt>Industry</dt>
+              <dd>Computer Software</dd>
+            </dl>
+          </div>
+        </div>
+        <Tabs defaultActiveKey={1} animation={false}>
+          <Tab eventKey={1} title='Background'>
+            <div className='resume-experience'>
+              <h3>Experience</h3>
+              <div>
+                <Image />
+                <h4>Gamania Digital Entertainment Co., Ltd.</h4>
+                <h5>Senior Programmer</h5>
+                <div>
+                  <time>April 2011</time> – <time>August 2014</time> (3 years 5 months)
+                </div>
+              </div>
+            </div>
+            <div className='resume-education'>
+              <h3>Education</h3>
+              <div>
+                <Image />
+                <h4>National Taipei University of Technology</h4>
+                <div>
+                  <dl>
+                    <dt>degree</dt>
+                    <dd>Bachelor's degree</dd>
+                    <dt>major</dt>
+                    <dd>Computer Science and Information Engineering</dd>
+                  </dl>
+                </div>
+                <div>
+                  <time>2005</time> – <time>2009</time>
+                </div>
+              </div>
+            </div>
+          </Tab>
+          <Tab eventKey={2} title='Skills'>skills</Tab>
+        </Tabs>
+      </Panel>
     )
   }
 }
