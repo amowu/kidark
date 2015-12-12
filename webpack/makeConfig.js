bf19aef5f1e7660152c2d893d232e721bc068c82
Amo Wu <amo260@gmail.com>
2015-12-13 01:28:49 +0800

    Add Font Awesome!

--- webpack/makeConfig.js
+++ webpack/makeConfig.js
@@ -46,9 +46,11 @@ export default function makeConfig (isDevelopment) {
       app: isDevelopment ? [
         `webpack-hot-middleware/client?path=http://${serverIp}:${constants.HOT_RELOAD_PORT}/__webpack_hmr`,
         'bootstrap-sass!' + path.join(constants.SRC_DIR, 'browser/theme/bootstrap.config.js'),
+        'font-awesome-webpack!' + path.join(constants.SRC_DIR, 'browser/theme/font-awesome.config.js'),
         path.join(constants.SRC_DIR, 'browser/main.js')
       ] : [
         'bootstrap-sass!' + path.join(constants.SRC_DIR, 'browser/theme/bootstrap.config.prod.js'),
+        'font-awesome-webpack!' + path.join(constants.SRC_DIR, 'browser/theme/font-awesome.config.prod.js'),
         path.join(constants.SRC_DIR, 'browser/main.js')
       ]
     },
@@ -64,11 +66,8 @@ export default function makeConfig (isDevelopment) {
     },
     module: {
       loaders: [
-        { test: /\.woff(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/font-woff' },
-        { test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/font-woff' },
-        { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&minetype=application/octet-stream' },
-        { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file' },
-        { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&minetype=image/svg+xml' },
+        { test: /\.woff(2)?(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/font-woff' },
+        { test: /\.(ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/, loader: 'file' },
         { test: /\.(gif|jpg|png)$/, loader: 'url?limit=100000' },
         {
           test: /\.(js|jsx)$/,
