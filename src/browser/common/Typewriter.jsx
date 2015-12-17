19e6031e21aa0c3988b91e64dc9f7652cb4dc0fa
Amo Wu <amo260@gmail.com>
2015-12-18 02:05:58 +0800

    Typewriter need default text

--- src/browser/common/Typewriter.jsx
+++ src/browser/common/Typewriter.jsx
@@ -14,6 +14,7 @@ export default class Typewriter extends Component {
     typeSpeed: PropTypes.number
   }
   static defaultProps = {
+    children: '',
     deleteSpeed: 50,
     loop: false,
     pauseDelay: 2000,
