be2fd8adaae5197a3a745fba63bf44f7dc4db7d2
Amo Wu <amo260@gmail.com>
2015-12-23 02:21:00 +0800

    Add first version that can click map to move player by A star pathfinding

--- src/browser/Game/states/GameState.js
+++ src/browser/Game/states/GameState.js
@@ -1,3 +1,6 @@
+import EasyStar from 'easystarjs'
+import _ from 'lodash'
+
 export default class GameState extends Phaser.State {
   init () {
     this.map = null
@@ -5,6 +8,12 @@ export default class GameState extends Phaser.State {
     this.player = null
     this.npc = null
     this.cursors = null
+    this.marker = null
+
+    this.playerNextMoveAllPath = null
+    this.playerNextMovePath = null
+    this.playerNextMoveX = 0
+    this.playerNextMoveY = 0
 
     this.world.setBounds(0, 0, 448, 496)
   }
@@ -39,42 +48,85 @@ export default class GameState extends Phaser.State {
     this.player.body.collideWorldBounds = true
     this.camera.follow(this.player)
 
+    this.playerNextMoveX = this.player.x
+    this.playerNextMoveY = this.player.y
+
     this.cursors = this.input.keyboard.createCursorKeys()
+
+    this.marker = this.add.graphics()
+    this.marker.lineStyle(2, 0xff0000, 1)
+    this.marker.drawRect(0, 0, 16, 16)
+
+    // for A* map
+    const grid = _.reduce(this.map.layers[0].data, (prev, curr) => {
+      prev.push(_.pluck(curr, 'index'))
+      return prev
+    }, [])
+    // setup A*
+    const easystar = new EasyStar.js()
+    easystar.setGrid(grid)
+    easystar.setAcceptableTiles([14])
+    // async function
+    const calculatePath = (start, end) => {
+      return new Promise((resolve) => {
+        easystar.findPath(start.x, start.y, end.x, end.y, path => {
+          resolve(path)
+        })
+        easystar.calculate()
+      })
+    }
+    this.map.findPath = async (start, end) => await calculatePath(start, end)
+    // mouse down
+    this.input.onDown.add(() => {
+      // player follow pathfinding
+      this.map.findPath({
+        x: this.layer.getTileX(this.player.x),
+        y: this.layer.getTileY(this.player.y)
+      }, {
+        x: this.layer.getTileX(this.marker.x),
+        y: this.layer.getTileY(this.marker.y)
+      }).then(path => {
+        // TODO: this.player.followPath(path)
+        console.log('pathfinding:', path)
+        if (path) {
+          this.playerNextMoveAllPath = path
+        }
+      })
+    })
   }
 
   update () {
     this.physics.arcade.collide(this.player, this.layer)
-
     this.physics.arcade.overlap(this.player, this.npc, (player, npc) => {
       npc.kill()
       this.npc = null
-      // TODO: Use better invoke solution
-      this.game.setCurrentDialogue('10001')
     })
 
-    if (this.cursors.left.isDown) {
-      this.player.scale.x = -1
-      this.player.angle = 0
-      this.player.body.velocity.x = -150
-      this.player.body.velocity.y = 0
-    } else if (this.cursors.right.isDown) {
-      this.player.scale.x = 1
-      this.player.angle = 0
-      this.player.body.velocity.x = 150
-      this.player.body.velocity.y = 0
-    } else if (this.cursors.up.isDown) {
-      this.player.scale.x = 1
-      this.player.angle = 270
-      this.player.body.velocity.x = 0
-      this.player.body.velocity.y = -150
-    } else if (this.cursors.down.isDown) {
-      this.player.scale.x = 1
-      this.player.angle = 90
-      this.player.body.velocity.x = 0
-      this.player.body.velocity.y = 150
-    } else {
-      this.player.body.velocity.x = 0
-      this.player.body.velocity.y = 0
+    if (this.playerNextMoveAllPath !== null && this.playerNextMovePath === null) {
+      console.log(this.playerNextMoveAllPath)
+      if (this.playerNextMoveAllPath.length === 0) {
+        this.playerNextMoveAllPath = null
+        this.playerNextMovePath = null
+        return
+      }
+      this.playerNextMovePath = this.playerNextMoveAllPath.shift()
+      console.log(this.playerNextMovePath)
+      this.playerNextMoveX = this.playerNextMovePath.x * 16
+      this.playerNextMoveY = this.playerNextMovePath.y * 16
+      console.log('player:', this.player.x, this.player.y)
+      console.log('next:', this.playerNextMoveX, this.playerNextMoveY)
+      this.physics.arcade.moveToXY(this.player, this.playerNextMoveX, this.playerNextMoveY)
     }
+
+    if (this.playerNextMovePath !== null && this.physics.arcade.distanceToXY(this.player, this.playerNextMoveX, this.playerNextMoveY) === 0) {
+      console.log('stop')
+      this.player.body.velocity.x = 0
+      this.player.body.velocity.y = 0
+      this.playerNextMovePath = null
+    }
+
+
+    this.marker.x = this.layer.getTileX(this.input.activePointer.worldX) * 16
+    this.marker.y = this.layer.getTileY(this.input.activePointer.worldY) * 16
   }
 }
