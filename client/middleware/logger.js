f73cdff5df2eba0655ebd02629d2723e0d6e7119
Amo Wu <amo260@gmail.com>
2016-04-07 16:41:47 +0800

    refactor: use redux-logger

--- client/middleware/logger.js
+++ client/middleware/logger.js
@@ -1,5 +1,6 @@
 
-export default store => next => action  => {
-  console.log(action)
-  return next(action)
-}
+import createLogger from 'redux-logger'
+
+const logger = createLogger()
+
+export default logger
